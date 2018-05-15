6e8acfbae8ca7727d1011afc4f53ebc194c90113
Roman <87jormen@gmail.com>
2018-05-14 21:56:52 -0400

    html

---
+++ public/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Door_to_Door</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="header-container">
+        <div class="logo">
+            <img src="img/logo.png" alt="logo">
+        </div>
+        <input type="checkbox" id="menu-checkbox">
+        <nav role="navigation">
+            <label for="menu-checkbox" class="toggle-button"
+                   data_open="MENU" data-close="CLOSE" onclick>
+            </label>
+            <ul class="main-menu">
+                <li><a href="#">H_O_M_E</a></li>
+                <li><a href="#">O_R_D_E_R</a></li>
+                <li><a href="#">T_I_M_E</a></li>
+                <li><a href="#">C_O_N_T_A_C_T</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<main>
+    <section class="top-slide">
+        <div class="top-container">
+            <div><h2>We Buket</h2></div>
+            <div><h2>obed</h2></div>
+            <div>
+                <p>Только для Вас</p>
+            </div>
+            <div>
+                <p>Только сегодня</p>
+            </div>
+            <div>
+                <p>Только сейчас</p>
+            </div>
+        </div>
+    </section>
+    <section class="specialize">
+        <h3></h3>
+    </section>
+</main>
+<script src="/js/script.js">
+</script>
+<script
+    src="http://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+</body>
+</html>
